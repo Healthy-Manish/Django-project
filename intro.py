adcd0121ef8f04556ac7cfbed781e493ea4b2929
Manish <manishdwivedi45165@gmail.com>
2023-08-01 21:10:10 +0530

    more files

---
+++ intro.py
@@ -0,0 +1,19 @@
+'''
+    What is Django?
+    1. python web framework
+    2. server side framework
+    3. designed to get you started fast
+    4. Batteries included
+    5. MVT(Model View Template) Design Pattern
+
+    What is a Web Framework ?
+   ** Collection of modules , packages and libraries Designed to 
+      speed up development
+
+    Backend Vs Frontend Frameworks
+    Backend (server side)--> 1.php 2.django 3.node.js 4.express
+    Frontend(client side)--> 1.Angular 2.React 3.View
+
+    Building API's with Django is easy!
+
+'''
